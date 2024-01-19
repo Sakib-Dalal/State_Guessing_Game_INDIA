7a73a67f343dca9b6fd9711da75eef263007cac6
Sakib_Dalal <sakibdalal73@gmail.com>
2024-01-19 22:58:57 +0530

    finnal project

---
+++ main.py
@@ -0,0 +1,43 @@
+import turtle
+import pandas
+
+image = "india.gif"
+
+screen = turtle.Screen()
+screen.title("Guess States of INDIA")
+screen.setup(width=800, height=900)
+screen.addshape(image)
+turtle.Turtle(image)
+
+
+# def get_mouse_click_coor(x, y):
+#     print(x, y)
+# turtle.onscreenclick(get_mouse_click_coor)
+# turtle.mainloop()
+
+data = pandas.read_csv('state_data.csv')
+all_states = data['states'].to_list()
+
+guess_states = []
+missed_states = []
+
+while len(guess_states) < 31:
+    user_input = turtle.textinput(f"Guess States {len(guess_states)}/30", "Enter State: ").title()
+
+    if user_input == "Exit":
+        for state in all_states:
+            if state not in guess_states:
+                missed_states.append(state)
+        print(missed_states)
+        break
+
+    if user_input in all_states:
+        guess_states.append(user_input)
+        my_pen = turtle.Turtle()
+        my_pen.color("blue")
+        my_pen.hideturtle()
+        my_pen.penup()
+        state_data = data[data['states'] == user_input]
+        my_pen.goto(int(state_data.X), int(state_data.Y))
+        my_pen.pendown()
+        my_pen.write(user_input, align='left', font=('Arial', 14, 'normal'))
